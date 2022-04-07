aa3df298a97833cb0b5471ffcfb14b50b8ac4e46
Rick Booth <admin@rickbooth.me>
2022-04-07 20:43:36 +0100

    feat: initial commit with packer

---
+++ lua/user/plugins.lua
@@ -0,0 +1,10 @@
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Plugins can have post-install/update hooks
+  use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = 'MarkdownPreview'}
+
+  -- Post-install/update hook with neovim command
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+end)
